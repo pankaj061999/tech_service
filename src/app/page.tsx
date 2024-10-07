95fe05ff9bb1df82d29aa4e2bdba653e7a7921e2
Pankaj Kumar Meena <pankajkmeena12@gmail.com>
2024-10-07 09:15:10 +0530

    Updated

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { BentoGridDemo } from "./components/Home";
+
+export default function Home() {
+  return (
+    <div className="mt-[120px] h-auto">
+      <BentoGridDemo />
+    </div>
+  );
+}
